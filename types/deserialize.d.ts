7278e5d2db29ba7e3c6fffaf75702a130656b5f5
James <750477+gdpelican@users.noreply.github.com>
2024-11-04 10:33:14 +1300

    Add types

---
+++ types/deserialize.d.ts
@@ -0,0 +1,5 @@
+declare function _exports({ data, included }: {
+    data: any;
+    included: any;
+}): any;
+export = _exports;
---
+++ types/main.d.ts
@@ -0,0 +1,2 @@
+export { deserialize };
+import deserialize = require("./deserialize");
